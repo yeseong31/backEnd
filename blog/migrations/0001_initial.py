cd0aa3fc56392ac96a25ddb787bfa9adcaecbd0b
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-04 17:54:02 +0900

    Update 0001_initial.py

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -16,8 +16,9 @@ class Migration(migrations.Migration):
             name='User',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=32)),
-                ('mail', models.EmailField(max_length=254)),
+                ('question', models.CharField(max_length=32)),
+                ('code', models.CharField(max_length=254)),
+                ('time', models.CharField(max_length=254)),
             ],
         ),
         migrations.CreateModel(
