ac193884099393cbb709340674498cc8efd1d8bb
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-08 10:43:02 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from rest_framework import routers
-from .views import UserViewSet, EntryViewSet
+from .views import EntryViewSet
+from django.urls import path
+from . import views
 
-
+app_name = 'blog'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+]
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'entries', EntryViewSet)
