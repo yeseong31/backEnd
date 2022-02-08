73ac92756b481e4c0ae8cc7993eae7695126967d
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-08 10:46:52 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
-"""config URL Configuration
+"""django_rest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -10,20 +10,17 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() kodeal: from django.urls import include, path
+    1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from blog.urls import router as blog_router
-from django.conf.urls import url, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('kodeal.urls')),                          # kodeal page
-    path('', RedirectView.as_view(url='/home/', permanent=True)),
-    path('common/', include('common.urls')),                        # common(login)
-    path('google/', include('allauth.urls')),                       # common(google login)
+    path('blog/', include('blog.urls')),
     url(r'^api/', include(blog_router.urls)),
 ]
