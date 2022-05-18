0eb3c258e24a435070e044a4717e588780f39307
예성 <yeseong31@naver.com>
2022-05-18 14:52:14 +0900

    0518 config/kodeal url 수정

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
     path('admin/', admin.site.urls),
     path('home/', include('kodeal.urls')),
-    path('', RedirectView.as_view(url='/home/', permanent=True)),
     path('common/', include('common.urls')),  # common(login)
     path('google/', include('allauth.urls')),  # common(google login)
     path('blog/', include('blog.urls')),    # kodeal page
