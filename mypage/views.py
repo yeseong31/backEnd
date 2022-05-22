8417adec74bf7c79bf4f462b550f16ba73dfb36a
예성 <yeseong31@naver.com>
2022-05-22 18:51:20 +0900

    0522 마이페이지 POST 접근 시 userid 전달

--- mypage/views.py
+++ mypage/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     if request.method == 'POST':
+        userid = request.POST.get('userid', None)
+
         return HttpResponse("마이페이지 테스트")
