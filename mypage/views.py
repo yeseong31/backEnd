23a28dd5796037a55bfa382d8f8db8c8fed8d07b
예성 <yeseong31@naver.com>
2022-05-22 19:18:04 +0900

    0522 마이페이지 index 구성

--- mypage/views.py
+++ mypage/views.py
@@ -1,10 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from blog.models import Keywords
+from common.models import User, Profile
 
-# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         userid = request.POST.get('userid', None)
 
-        return HttpResponse("마이페이지 테스트")
+        # 해당 userid를 가지는 사용자가 존재한다면
+        if User.objects.filter(userid=userid).exists():
+            user = User.objects.get(userid=userid)
+            # 마이페이지에 대한 정보(이미지)가 있다면
+            if Profile.objects.filter(userid=userid).exists():
+                profile = Profile.objects.get(userid=userid)
+                context = {
+                    'image': profile.path,
+                    'info': {
+                        'email': user.email,
+                        'questionCount': 0
+                    },
+                    'keywords': 0
+                }
+            # 마이페이지에 대한 정보가 없으므로 빈 화면 출력
+            else:
+                context = None
+            return render(request, 'mypage/index.html', context)
+
+        # 해당 userid를 가지는 사용자가 없으므로 에러 반환
+        else:
+            return JsonResponse({'message': 'This ID does not exist.', 'status': 400}, status=400)
