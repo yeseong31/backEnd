a1e3a62e738a5ba79107cdd5f4bf19cf77745193
예성 <yeseong31@naver.com>
2022-05-22 18:20:38 +0900

    0522 테스트 페이지 로그인 기능 구현

--- common/urls.py
+++ common/urls.py
@@ -8,12 +8,14 @@ urlpatterns = [
     path('', index, name='index'),
 
     # 회원가입
-    path('signup/', SignupView.as_view(), name='signup'),  # Json으로 회원가입 (react에서 사용)
+    path('signup/', SignupView.as_view(), name='signup'),  # Json으로 회원가입 (react용)
     path('signup/comp/', signup_complete, name='signup_complete'),
 
     # 일반 로그인
-    path('login/', LoginView.as_view(), name='login_main'),  # Json으로 로그인 (react에서 사용)
+    path('login/', LoginView.as_view(), name='login_main'),  # Json으로 로그인 (react용)
+    path('login/test/', LoginTestView.as_view(), name='login_main_test'),  # HTML5로 로그인 (backend 테스트용)
     path('logout/', logout_main, name='logout_main'),
+
 
     # 아이디 중복 확인
     path('signup/check/id/', CheckID.as_view(), name='duplicate_id_check'),
