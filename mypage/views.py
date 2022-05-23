e28b3e2191ba398cb5336e942de227db9d5f4b8d
yeseong31 <yeseong31@naver.com>
2022-05-23 21:47:30 +0900

    0523 마이페이지에 JSON 전달

--- mypage/views.py
+++ mypage/views.py
@@ -1,7 +1,10 @@
+import collections
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from blog.models import Keywords
+from blog.models import User as Question
 from common.models import User, Profile
 
 
@@ -12,22 +15,54 @@ def index(request):
         # 해당 userid를 가지는 사용자가 존재한다면
         if User.objects.filter(userid=userid).exists():
             user = User.objects.get(userid=userid)
+
+            # user의 질문 수 계산
+            question_count = count_questions(user)
+            # user의 키워드 빈도 수 계산
+            keyword_cnt_info = check_freq_keyword(user)
+
             # 마이페이지에 대한 정보(이미지)가 있다면
+            path = None
             if Profile.objects.filter(userid=userid).exists():
-                profile = Profile.objects.get(userid=userid)
-                context = {
-                    'image': profile.path,
-                    'info': {
-                        'email': user.email,
-                        'questionCount': 0
-                    },
-                    'keywords': 0
-                }
-            # 마이페이지에 대한 정보가 없으므로 빈 화면 출력
-            else:
-                context = None
-            return render(request, 'mypage/index.html', context)
+                path = Profile.objects.get(userid=userid).path
+
+            context = {
+                'image': path,
+                'info': {
+                    'email': user.email,
+                    'questionCount': question_count
+                },
+                'keywords': keyword_cnt_info
+            }
+
+            # print(context)
+            # return render(request, 'mypage/index.html', context)
+            return JsonResponse({'context': context, 'status': 200}, status=200)
 
         # 해당 userid를 가지는 사용자가 없으므로 에러 반환
         else:
             return JsonResponse({'message': 'This ID does not exist.', 'status': 400}, status=400)
+
+
+def count_questions(user):
+    questions_obj = Question.objects.filter(userid=user).all()
+    return len(questions_obj)
+
+
+# 전달받은 user의 키워드 빈도 수 계산
+def check_freq_keyword(user):
+    keywords_obj = Keywords.objects.filter(userid=user).all()
+
+    keyword_list = []
+    for keyword_obj in keywords_obj:
+        target = keyword_obj.keyword
+        keyword_list.append(target)
+
+    result = collections.Counter(keyword_list).most_common(10)
+    # print(result)
+    return result
+
+
+# 마이페이지에 사용자 키워드 통계 출력
+def calcul_user_keyword_statistics():
+    pass
