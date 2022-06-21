3c09605e1b794ac64099f7cf1c6c4e98e4ca8547
예성 <yeseong31@naver.com>
2022-06-21 15:20:30 +0900

    0621 DB 테스트 데이터 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.utils import timezone
+
+from blog.models import User as Question
+from common.models import User
+
+for i in range(5, 101):
+    Question(
+        question=f'테스트 질문...{i}',
+        question_papago=f'Test Question...{i}',
+        code=f'result code...{i}',
+        star=5,
+        language='Python 3' if i < 50 else 'Javascript',
+        preprocess='...',
+        userid=User(userid='kjh')
+    ).save()
