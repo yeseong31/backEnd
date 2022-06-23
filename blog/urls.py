8ed1cb8956a4cb67d3d0c06cc04366be6cd9c9de
예성 <yeseong31@naver.com>
2022-06-23 14:30:42 +0900

    0623 수정

--- blog/urls.py
+++ blog/urls.py
@@ -8,11 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:id>/', views.IndexView.as_view()),
 
-    # 03/31 부로 kodeal 디렉터리의 내용을 blog 디렉터리로 통합
-
-    # 05.22 keyward
-    # path('key_word/', views.key_word, name='key_word'),
-
     # QnA화면
     path('qna_main/', views.qna_main, name='qna_main'),
     path('qna_answer/', views.qna_answer, name='qna_answer'),
